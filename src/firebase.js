dcb054a282f7be4201d8c5d10ba8d8d583aebb90
raikk <raik.kh@gmail.com>
2024-08-15 08:38:59 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBL5MnzGqZ0IEJ5Lr12HdPWcrkQm5tSomg",
+  apiKey: "*****************",
   authDomain: "nielit-course-d6dea.firebaseapp.com",
   projectId: "nielit-course-d6dea",
   storageBucket: "nielit-course-d6dea.appspot.com",
-  messagingSenderId: "174590292790",
-  appId: "1:174590292790:web:1c97c271380a3395cbb997",
-  measurementId: "G-J040GW5HZM"
+  messagingSenderId: "174590292****",
+  appId: "1:174590292790:web:1c97c271380a3**************7",
+  measurementId: "G-J040*******"
 };
 
 // Initialize Firebase
